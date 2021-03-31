37104458632daf1672ff3682930097205d32123b
Mercy Osebe <marcynyaata@gmail.com>
2021-03-31 17:33:10 +0300

    My FItness App first commit

---
+++ app/src/main/java/com/example/fitness101/BMILogic.java
@@ -0,0 +1,16 @@
+package com.example.fitness101;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class BMILogic extends AppCompatActivity{
+
+}
--- app/src/main/java/com/example/fitness101/MainActivity.java
+++ app/src/main/java/com/example/fitness101/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.fitness101;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,8 +14,15 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText wei;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +31,50 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        wei =findViewById(R.id.editWeight);
+        EditText hei=findViewById(R.id.editHeight);
+        TextView tvResults =findViewById(R.id.textResults);
+        Button butt=findViewById(R.id.buttonNext);
+
+        Button but = findViewById(R.id.buttonBMI);
+
+        but.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                dismisskeyboard();
+
+                String stringWei=wei.getText().toString();
+                float intWei=Float.parseFloat(stringWei);
+
+                String stringHei=hei.getText().toString();
+                float intHei=Float.parseFloat(stringHei);
+
+                float bmi;
+                bmi=intWei/(intHei*intHei);
+
+
+                String res = Float.toString(bmi);
+                tvResults.setText(res);
+                Toast.makeText(MainActivity.this, "This is your BMI", Toast.LENGTH_SHORT).show();
+                if(bmi<10){}
+
+
+            }
+
+        });
+        butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,BMIExplained.class);
+                startActivity(intent);
             }
         });
     }
 
+    private void dismisskeyboard() {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(wei.getWindowToken(), 0);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
